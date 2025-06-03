71a3c7a00920b77c6679ad8c92ea20fcbc3082ea
djeanner <djeanner@users.noreply.github.com>
2025-06-03 14:33:08 +0200

    clean up code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
 #include "secrets.h"
 
-// in secret.h set the ip of the server
-/*
-const char* SERVER_HOST = "AAA.BBB.CCC.DDD"; // replace with the ip number
-const int SERVER_PORT = 3000;
-*/
-
 // for pi pico with a gravity uart (AIR7880EU chip) 4G CAT I mocules V1.0 connected with
 // gravity board / pi pico
 // GND black/ 37
-// VCC red  / 35
+// VCC red  / 40
 // Rx blue  / 1
 // Tx green / 2
 
@@ -25,7 +19,7 @@ const int ledPinBlink = 25;  // Onboard LED pin on Raspberry Pi Pico
 #endif
 
 #ifdef ENABLE_USB_SERIAL
-#define SerUSB Serial  // rename to facilitate debug Serial is the USB ser port
+#define SerUSB Serial  // Renamed to facilitate and debug (Serial is the USB ser port)
 #endif
 
 void logToSerial(const char* msg, const bool endOfLine = true) {
@@ -85,8 +79,7 @@ void sendHTTPRequest(const char* host, const char* path, const char* payload) {
   sendCommand("AT+CIPSEND", 2000);
   delay(500);
 
-  String request =
-    String("POST ") + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Content-Type: application/x-www-form-urlencoded\r\n" + "Content-Length: " + String(strlen(payload)) + "\r\n" + "Connection: close\r\n\r\n" + payload;
+  String request = String("POST ") + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Content-Type: application/x-www-form-urlencoded\r\n" + "Content-Length: " + String(strlen(payload)) + "\r\n" + "Connection: close\r\n\r\n" + payload;
 
   AIR780.print(request);
   AIR780.write(0x1A);
@@ -132,7 +125,6 @@ void shutdownModem() {
   sendCommand("AT+CIPSHUT", 2000);   // Shut IP stack
   sendCommand("AT+CGATT=0", 1000);   // Detach from GPRS
   sendCommand("AT+CFUN=0", 1000);    // Minimum functionality mode
-
   shortBlinks(2);  // Feedback
   logToSerial("Modem in low-power mode.");
 }
@@ -153,18 +145,13 @@ void wakeModem() {
   }
 }
 
-
-
 void flushSerialInput() {
 #ifdef ENABLE_USB_SERIAL
-
   while (SerUSB.available()) {
     SerUSB.read();  // Discard all incoming serial data
   }
 #endif
 }
-
-
 
 bool connectTCP(const char* host, int port) {
   String cmd = String("AT+CIPSTART=\"TCP\",\"") + String(host) + "\"," + port;
@@ -194,15 +181,10 @@ bool connectTCP(const char* host, int port) {
   return false;
 }
 
-
-
 void sendHTTPRequestOLDnotWorking(const char* host) {
   flushSerialInput();  // ✅ Wipe out any leftover serial input
-  //sendCommand("AT+CIPSEND", 1000);
-  //delay(500);
 
   while (AIR780.available()) AIR780.read();
-
 
   String request = String("AT+CIPSTART=\"TCP\",\"") + String(host) + String("\",80");  // Connect to ESP32 IP
   AIR780.println(request);
@@ -217,8 +199,7 @@ void sendHTTPRequestOLDnotWorking(const char* host) {
   logToSerial(request.c_str());
   logToSerial("[HTTP request sent]");
 
-  //AIR780.println("GET / HTTP/1.1\r\nHost: 192.168.8.139\r\nConnection: keep-alive\r\n\r\n"); // HTTP GET request
-  //String httpRequest = String("GET / HTTP/1.1\r\nHost: \"") + host + String("\"\r\nConnection: keep-alive\r\n\r\n");//close
+  //String httpRequest = String("GET / HTTP/1.1\r\nHost: \"") + String(host) + String("\"\r\nConnection: keep-alive\r\n\r\n");//close
   String httpRequest = String("GET / HTTP/1.1\r\nHost: \"") + String(host) + String("\"\r\nConnection: close\r\n\r\n");  //close
   AIR780.print(httpRequest);
   delay(1000);  // Wait for the request to be sent
@@ -239,7 +220,6 @@ void sendHTTPRequestOLDnotWorking(const char* host) {
   logToSerial(fullMessage.c_str());
   logToSerial("[Server Response End]");
 }
-
 
 void disconnectTCP() {
   sendCommand("AT+CIPCLOSE", 1000);
@@ -363,7 +343,6 @@ stat value	Meaning
   return false;
 }
 
-
 void configureAPN(const char* apn) {
   String cmd = String("AT+CGDCONT=1,\"IP\",\"") + apn + "\"";
   sendCommand(cmd.c_str());
@@ -376,7 +355,6 @@ void attachGPRS() {
   sendCommand("AT+CGPADDR=1");
   delay(1000);
 }
-
 
 void sendDataOnce(const char* data) {
   if (!waitForReady()) {
@@ -408,8 +386,6 @@ void sendDataOnce(const char* data) {
     shortBlinks(5, true);
   }
 }
-
-
 
 void setup() {
 #ifdef ENABLE_BLINK
@@ -452,10 +428,10 @@ void setup() {
 void loop() {
   wakeModem();  // Power up network stack
   counter++;
-  String mainData = "device=AIR780&set=2&value=" + String(counter);
-  sendDataOnce(mainData.c_str());  // Send to server
+  String mainData = "device=AIR780&set=3&value=" + String(counter);
+  sendDataOnce(mainData.c_str());  // Send to server up to 200–500 mA for AIT780EU
 
-  shutdownModem();  // Shut everything down
+  shutdownModem();  // Shut everything down ~5–10 mA for AIT780EU
 
   //logToSerial("Sleeping for 1 hour...");delay(3600000);  // Wait 1 hour (60 min × 60 sec × 1000 ms)
   logToSerial("Sleeping for 1 min...");
