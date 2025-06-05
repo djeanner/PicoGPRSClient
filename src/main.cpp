267b64d188de68e89a6e2809e6314a503556823b
djeanner <djeanner@users.noreply.github.com>
2025-06-05 12:37:57 +0200

    updated main gsm code for gps

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,37 @@
 #include "secrets.h"
 
 
+// MAIN MACROS
+#define GPS_MODULE_DATA
+#define AIR780_MODULE
+
+#ifdef GPS_MODULE_DATA            // For SIM868 Using powerpins 36-40, 1,2,6,7,19,22 one groud (18,23)
+#define UART_PORT_SIM868 Serial1  // may need to change to Serial1 if change port (see below)
+#define UART_PORT_SIM868TX 0      // GPIO0 pin1
+#define UART_PORT_SIM868RX 1      // GPIO1 pin2
+#define PWR_EN_SIM868 14          // GPI14 pint 19
+#endif
+
+#ifdef AIR780_MODULE  // For AIR780
 // for pi pico with a gravity uart (AIR7880EU chip) 4G CAT I mocules V1.0 connected with
 // gravity board / pi pico
 // GND black/ pin 37
 // VCC red  / pin 40
 // Rx blue  / pin 1 GPIO0 Serial1 or pin 6 GPIO4 Serial2 see setTX
 // Tx green / pin 2 GPIO1 Serial1 or pin 7 GPIO5 Serial2 see setRX
-#define AIR780 Serial2 // Serial1 or Serial2
-#define AIR780SER_TX 4 // 0 GPIO0 pin 1 for Serial1 or 4 GPIO4 pin 6 for Serial2
-#define AIR780SER_RX 5 // 1 GPIO0 pin 2 for Serial1 or 5 GPIO4 pin 7 for Serial2
+#define AIR780 Serial2  // Serial1 or Serial2
+#define AIR780SER_TX 4  // 0 GPIO0 pin 1 for Serial1 or 4 GPIO4 pin 6 for Serial2
+#define AIR780SER_RX 5  // 1 GPIO0 pin 2 for Serial1 or 5 GPIO4 pin 7 for Serial2
+#endif
 
-int setNumber = 100; // sent to server
+int setNumber = 100;  // sent to server
 bool verbose = true;  // Set to false to reduce serial prints
 unsigned long counter = 0;
 const String deviceName = "AIR780";
 
 #define ENABLE_BLINK  // Comment this line to disable LED blinking
 // #define ENABLE_USB_SERIAL  // Comment this line to disable terminal connection to usb serial port
-#define SAVE_SET_EEPROM // disable to avoid using flash memory
+#define SAVE_SET_EEPROM  // disable to avoid using flash memory
 
 #ifdef SAVE_SET_EEPROM
 #include <EEPROM.h>
@@ -31,6 +44,171 @@ const int ledPinBlink = 25;  // Onboard LED pin on Raspberry Pi Pico
 #ifdef ENABLE_USB_SERIAL
 #define SerUSB Serial  // Renamed to facilitate and debug (Serial is the USB ser port)
 #endif
+
+#ifdef GPS_MODULE_DATA  // For SIM868
+
+class GpsInfo {
+public:
+  int runStatus = 0;
+  int fixStatus = 0;
+  String utc;
+  double lat = 0.0;
+  double lon = 0.0;
+  double alt = 0.0;
+  double speed = 0.0;
+  double course = 0.0;
+  int fixMode = 0;
+  double hdop = 0.0;
+  double pdop = 0.0;
+  double vdop = 0.0;
+  int satsInView = 0;
+  int satsUsed = 0;
+  int glonassUsed = 0;
+  int cn0max = 0;
+  int numberValidGPSdata = 0;
+  void fromCGNSINF(const String& infoLine) {
+    String fields[22];
+    int start = 0, end = 0, index = 0;
+    while ((end = infoLine.indexOf(',', start)) != -1 && index < 21) {
+      fields[index++] = infoLine.substring(start, end);
+      start = end + 1;
+    }
+    fields[index] = infoLine.substring(start);  // last field
+
+    runStatus = fields[0].toInt();
+    fixStatus = fields[1].toInt();
+    utc = fields[2];
+    lat = fields[3].toDouble();
+    lon = fields[4].toDouble();
+    alt = fields[5].toDouble();
+    speed = fields[6].toDouble();
+    course = fields[7].toDouble();
+    fixMode = fields[8].toInt();
+    hdop = fields[10].toDouble();
+    pdop = fields[11].toDouble();
+    vdop = fields[12].toDouble();
+    satsInView = fields[14].toInt();
+    satsUsed = fields[15].toInt();
+    glonassUsed = fields[16].toInt();
+    cn0max = fields[19].toInt();
+  }
+
+  String toJson() const {
+    return String("{") + "\"runStatus\":" + String(runStatus) + "," + "\"fixStatus\":" + String(fixStatus) + "," + "\"utc\":\"" + utc + "\"," + "\"lat\":" + String(lat, 6) + "," + "\"lon\":" + String(lon, 6) + "," + "\"alt\":" + String(alt, 3) + "," + "\"speed\":" + String(speed, 2) + "," + "\"course\":" + String(course, 1) + "," + "\"fixMode\":" + String(fixMode) + "," + "\"hdop\":" + String(hdop, 1) + "," + "\"pdop\":" + String(pdop, 1) + "," + "\"vdop\":" + String(vdop, 1) + "," + "\"satsInView\":" + String(satsInView) + "," + "\"satsUsed\":" + String(satsUsed) + "," + "\"glonassUsed\":" + String(glonassUsed) + "," + "\"cn0max\":" + String(cn0max) + "}";
+  }
+
+  String toAmpersandString() const {
+    return "runStatus=" + String(runStatus) + "&" + "fixStatus=" + String(fixStatus) + "&" + "utc=" + utc + "&" + "lat=" + String(lat, 6) + "&" + "lon=" + String(lon, 6) + "&" + "alt=" + String(alt, 3) + "&" + "speed=" + String(speed, 2) + "&" + "course=" + String(course, 1) + "&" + "fixMode=" + String(fixMode) + "&" + "hdop=" + String(hdop, 1) + "&" + "pdop=" + String(pdop, 1) + "&" + "vdop=" + String(vdop, 1) + "&" + "satsInView=" + String(satsInView) + "&" + "satsUsed=" + String(satsUsed) + "&" + "glonassUsed=" + String(glonassUsed) + "&" + "cn0max=" + String(cn0max);
+  }
+
+  bool isValidFix() const {
+    return fixStatus == 1;
+  }
+};
+
+void powerOnOffSIM868() {
+  digitalWrite(PWR_EN_SIM868, HIGH);
+  delay(2000);
+  digitalWrite(PWR_EN_SIM868, LOW);
+}
+
+String waitRespInfoSIM868(unsigned long timeout = 2000) {
+  unsigned long start = millis();
+  String response = "";
+  while (millis() - start < timeout) {
+    while (UART_PORT_SIM868.available()) {
+      response += (char)UART_PORT_SIM868.read();
+    }
+  }
+  //SerialUSBSIM868.println(response);
+  return response;
+}
+
+bool sendATSIM868(const String& cmd, const String& expected, unsigned long timeout = 2000) {
+  UART_PORT_SIM868.println(cmd);
+  String response = "";
+  unsigned long start = millis();
+  while (millis() - start < timeout) {
+    while (UART_PORT_SIM868.available()) {
+      response += (char)UART_PORT_SIM868.read();
+    }
+    if (response.indexOf(expected) != -1) {
+      //SerialUSBSIM868.println(response);
+      return true;
+    }
+  }
+  //SerialUSBSIM868.println(cmd + " back:\t" + response);
+  return false;
+}
+
+void checkStartSIM868() {
+  while (true) {
+    UART_PORT_SIM868.println("ATE1");
+    delay(2000);
+    UART_PORT_SIM868.println("AT");
+    String resp = waitRespInfoSIM868();
+    if (resp.indexOf("OK") != -1) {
+      //SerialUSBSIM868.println("SIM868 is ready");
+      break;
+    } else {
+      powerOnOffSIM868();
+      //SerialUSBSIM868.println("SIM868 is starting up, please wait...");
+      delay(8000);
+    }
+  }
+}
+
+String getGPSInfoSIM868() {
+  //SerialUSBSIM868.println("Start GPS session...");
+
+  if (!sendATSIM868("AT+CGNSPWR=1", "OK")) {
+    //SerialUSBSIM868.println("Failed to power on GPS module.");
+    return "";
+  }
+
+  delay(2000);
+  const int maxRetries = 10;
+  int numberValidGPSdata = 0;
+  GpsInfo bestFix;
+  float bestHdop = 99.9;
+
+  for (int i = 0; i < maxRetries; ++i) {
+    UART_PORT_SIM868.println("AT+CGNSINF");
+    String response = waitRespInfoSIM868();
+    int idx = response.indexOf("+CGNSINF:");
+    if (idx != -1) {
+      String dataLine = response.substring(idx + 9);
+      dataLine.trim();
+
+      GpsInfo currentFix;
+      currentFix.fromCGNSINF(dataLine);
+
+      if (currentFix.isValidFix() && currentFix.hdop < bestHdop) {
+        bestFix = currentFix;
+        bestHdop = currentFix.hdop;
+        numberValidGPSdata++;
+        bestFix.numberValidGPSdata = numberValidGPSdata;
+        //SerialUSBSIM868.println("GPS quality improved >>>");
+      } else {
+        //SerialUSBSIM868.println("GPS quality NOT improved >>>");
+      }
+    }
+
+    delay(2000);
+  }
+
+  sendATSIM868("AT+CGNSPWR=0", "OK");
+
+  if (bestFix.isValidFix()) {
+    return bestFix.toAmpersandString();  // or toJson() depending on use case
+  } else {
+    //SerialUSBSIM868.println("❌ No valid GPS fix found.");
+    return "";
+  }
+}
+
+#endif
+
 
 void logToSerial(const char* msg, const bool endOfLine = true) {
 #ifdef ENABLE_USB_SERIAL
@@ -135,7 +313,7 @@ void shutdownModem() {
   sendCommand("AT+CIPSHUT", 2000);   // Shut IP stack
   sendCommand("AT+CGATT=0", 1000);   // Detach from GPRS
   sendCommand("AT+CFUN=0", 1000);    // Minimum functionality mode
-  shortBlinks(2);  // Feedback
+  shortBlinks(2);                    // Feedback
   logToSerial("Modem in low-power mode.");
 }
 
@@ -399,11 +577,23 @@ void sendDataOnce(const char* data) {
 
 void setup() {
 
+#ifdef GPS_MODULE_DATA  // For SIM868 INIT SIM868
+  pinMode(PWR_EN_SIM868, OUTPUT);
+  digitalWrite(PWR_EN_SIM868, LOW);
 
+  UART_PORT_SIM868.setTX(UART_PORT_SIM868TX);  // GPIO0
+  UART_PORT_SIM868.setRX(UART_PORT_SIM868RX);  // GPIO1
+  UART_PORT_SIM868.begin(115200);              // SIM868 default
+  sendATSIM868("AT+SLED=0", "OK");             // Disable status LED set to 1 to set back
+
+  // checkStartSIM868();
+  powerOnOffSIM868();  // Pulses PWRKEY
+
+#endif
 
 //////////////////////////////////////// save data in EEPROM // start
 #ifdef SAVE_SET_EEPROM
-  EEPROM.begin(512); // Initialize 512 bytes
+  EEPROM.begin(512);  // Initialize 512 bytes
   const int addr = 0;
   const int magicAddr = addr + sizeof(int);
   const int MAGIC_VALUE = 0xABC0;
@@ -422,9 +612,9 @@ void setup() {
     EEPROM.put(addr, 0);
     EEPROM.put(magicAddr, MAGIC_VALUE);
   }
-  EEPROM.commit(); // Only one commit needed
+  EEPROM.commit();  // Only one commit needed
 #endif
-//////////////////////////////////////// save data in EEPROM // end
+  //////////////////////////////////////// save data in EEPROM // end
 
 
 
@@ -437,9 +627,9 @@ void setup() {
   while (!SerUSB)
     ;
 #endif
-  AIR780.setTX(AIR780SER_TX);       // 0: GPIO0 or 4:GPIO4
-  AIR780.setRX(AIR780SER_RX);       // 1: GPIO1 or 5:GPIO5
-  AIR780.begin(115200);  // Default for AIR780
+  AIR780.setTX(AIR780SER_TX);  // 0: GPIO0 or 4:GPIO4
+  AIR780.setRX(AIR780SER_RX);  // 1: GPIO1 or 5:GPIO5
+  AIR780.begin(115200);        // Default for AIR780
 
   logToSerial("Initializing AIR780 module...");
   delay(2000);
@@ -462,24 +652,28 @@ void setup() {
 #ifdef ENABLE_BLINK
   shortBlinks(3);
 #endif
-
   //runDiagnostics();
 }
 void loop() {
   counter++;
 
-  wakeModem();  // Power up network stack
-
-  String mainData = ""; 
+  String mainData = "";
   String separator = "&";
   mainData += "device=" + deviceName;
   mainData += separator;
   mainData += "setNumber=" + String(setNumber);
   mainData += separator;
   mainData += "value=" + String(counter);
-  sendDataOnce(mainData.c_str());  // Send to server up to 200–500 mA for AIT780EU
 
-  shutdownModem();  // Shut everything down ~5–10 mA for AIT780EU
+#ifdef GPS_MODULE_DATA  // For SIM868
+  String gpsString = getGPSInfoSIM868();
+  mainData += separator;
+  mainData += "GPS=" + String(gpsString);
+#endif
+
+  wakeModem();                     // Power up network stack
+  sendDataOnce(mainData.c_str());  // Send to server up to 200–500 mA for AIT780EU
+  shutdownModem();                 // Shut everything down ~5–10 mA for AIT780EU
 
   logToSerial("Sleeping for 1 min...");
   delay(60000);  // Wait 1 min (60 sec × 1000 ms)
